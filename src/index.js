a0c89339e8540c9a621ad0028830eff5048cf494
bvasic <vasicbie@gmail.com>
2019-03-02 15:39:02 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'Arial'
+}
+
+const title = React.createElement(
+    'h1',
+    {id: 'title', className: 'header'},
+    'Hello World'
+)
+
+ReactDOM.render(
+    title,
+    document.getElementById('root')
+)
