2e6b280a71e8dd98ba9926784436368b2ab39370
bvasic <vasicbie@gmail.com>
2019-03-03 13:47:37 +0000

    Rendering array into virtual DOM by mapping each key-value pair

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+let bookList = [
+    {"title": "Hunger", "author":"Roxane Gray", "pages": 320},
+    {"title": "The Sun Also Rises", "author":"Ernest Hemingway", "pages": 260},
+    {"title": "White Teeth", "author":"Zadie Smith", "pages": 480},
+    {"title": "Cat's Cradle", "author":"Ernest Hemingway", "pages": 260}
+]
+
 const Book = ({title, author, pages}) => {
     return (
         <section>
@@ -11,18 +18,23 @@ const Book = ({title, author, pages}) => {
     )
 }
 
-const Library = () => {
+const Library = ({books}) => {
     return (
         <div>
-            <Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260}/>
-            <Book title="White Teeth" author="Zadie Smith" pages={260}/>
-            <Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304}/>
+            {books.map(
+                (book, i) => 
+                        <Book 
+                            key={i}
+                            title={book.title} 
+                            author={book.author} 
+                            pages={book.pages}/>
+            )}
         </div>
     )
 }
 
 render(
-    <Library />,    
+    <Library books={bookList}/>,    
     document.getElementById('root')
 )
 
