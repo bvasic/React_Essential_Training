bf894c5a9b9d6979f7b1845460a280a91ddc3971
bvasic <vasicbie@gmail.com>
2019-03-03 11:53:14 +0000

    Creating custom methods

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
 let skiData = {
     total: 50,
@@ -9,27 +9,34 @@ let skiData = {
 }
 
 class SkiDayCounter extends Component{
+    getPercent = decimal => {
+        return decimal * 100 + '%'
+    }
+    calcGoalProgress = (total,goal) => {
+        return this.getPercent(total/goal)
+    }
     render() {
+        const { total, powder, backcountry, goal } = this.props
         return (
             <section>
                 <div>
-                    <p>Total days: {this.props.total}</p>
+                    <p>Total days: {total}</p>
                 </div>
                 <div>
-                    <p>Powder days: {this.props.powder}</p>
+                    <p>Powder days: {powder}</p>
                 </div>
                 <div>
-                    <p>Backcountry days: {this.props.backcountry}</p>
+                    <p>Backcountry days: {backcountry}</p>
                 </div>
                 <div>
-                    <p>Goal: {this.props.goal}</p>
+                    <p>Goal progress: {this.calcGoalProgress(total,goal)}</p>
                 </div>
             </section>
         )
     }
 }
 
-ReactDOM.render(
+render(
     <SkiDayCounter 
         total={skiData.total}
         powder={skiData.powder}
